cb2ada4f72d50c1833331aec89bb9e6aac5be22b
SilvaHaberl <silva.haberl1989@gmail.com>
2015-12-14 06:26:06 +0100

    svojstva(properties), private konstruktor, konstruktor s parametrima

---
+++ Fejs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fejs
+{
+
+    
+    class Osoba {
+
+        private string ime;
+        private string prezime;
+
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+        private Osoba() { }
+
+        Osoba(string _ime, string _prezime)
+        {
+
+            ime = _ime;
+            prezime = _prezime;
+
+        }
+
+
+
+     
+    
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
